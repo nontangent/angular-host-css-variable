ecf6ea00b651481a0f8bb8d08080a0b39a2f801f
nontangent <irregular0number@gmail.com>
2021-01-24 01:31:26 +0900

    feat: add support host-css-variable@1.2.0

--- src/custom-webpack.ts
+++ src/custom-webpack.ts
@@ -1,7 +1,7 @@
 export default function (config, ..._: any[]) {
 	config.module.rules = config.module.rules.map((rule: any) => {
 		if (rule.test.toString() == /\.scss$|\.sass$/.toString()) {
-			rule.use = [...rule.use, 'host-css-variable/loader'];
+			rule.use = [...rule.use, 'host-css-variable'];
 		}
 
 		return rule;
